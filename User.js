7e23bd0553fb4cb17d5a848fe942d970a87dd757
Prasanna Patel <patel.prasanna2396@gmail.com>
2023-11-06 23:10:08 -0500

    Intial Commit

---
+++ User.js
@@ -0,0 +1,22 @@
+const mongoose = require ('mongoose');
+const UserSchema = new mongoose.Schema({
+    
+    LastName: {
+        type: String,
+        required: true
+      },
+    FirstName: {
+        type: String,
+        required: true
+      },
+      Email:{
+        type:String,
+        required: true
+      },
+      Password: {
+        type: String,
+        required: true
+      }
+})
+const Usermodel =  mongoose.model("User", UserSchema)
+module.exports = Usermodel
